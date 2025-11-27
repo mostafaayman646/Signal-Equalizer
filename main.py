8413f7167e0b8753ed6db33dd56058343f8bb0de
Amr Ahmed <148648946+Amr2054@users.noreply.github.com>
2025-11-27 09:55:09 +0200

    JSON Files Update

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ from components.callbacks.Cine_Viewer_Full import register_cine_viewer_callbacks
 # Import other components and callbacks
 from components.layout_builder import create_app_layout
 from components.tab_callbacks import register_tab_callbacks
-
 from components.callbacks.Upload_signal import register_Upload_signal
 from components.callbacks.NavBar_Mode_Switch import register_mode_switch
 from components.callbacks.Toggle_spectogram import register_Toggle_spectogram
 from components.callbacks.PlayAudio import register_PlayAudio
 from components.callbacks.Download_Audio import register_Download_audio
 from components.callbacks.Scale_toggle import register_Scale_toggle
-
 from modes.customized.customized_callbacks.process_sliders_callbacks import register_customized_callbacks
 from modes.customized.customized_callbacks.Render_Ai_models_callbacks import register_ai_models
 from modes.customized.customized_callbacks.Musical_Ai_model_callbacks import register_Musical_AiModel
